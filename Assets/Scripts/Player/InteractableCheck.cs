be04c755d6a51f31a00998afc261791e38d30536
Lazea <lazeaai@gmail.com>
2023-10-19 17:12:40 -0400

    Updated test scene with new features

--- Assets/Scripts/Player/InteractableCheck.cs
+++ Assets/Scripts/Player/InteractableCheck.cs
@@ -39,7 +39,8 @@ public class InteractableCheck : MonoBehaviour
     {
         selectedProp = null;
         selectedInteractable = null;
-        UIManager.Instance.HideInteractionPrompt();
+        if(UIManager.Instance != null)
+            UIManager.Instance.HideInteractionPrompt();
 
         StopCoroutine("PickupProp");
     }
