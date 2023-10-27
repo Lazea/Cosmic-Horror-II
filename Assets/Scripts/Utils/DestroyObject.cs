953c980890d9eaec111a70e87a7f3ce25e5ee43e
Lazea <lazeaai@gmail.com>
2023-10-27 01:10:07 -0400

    Added VO lines

---
+++ Assets/Scripts/Utils/DestroyObject.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestroyObject : MonoBehaviour
+{
+    public void DestroyThisObject()
+    {
+        Destroy(gameObject);
+    }
+
+    public void DestroyThisObjectWithDelay(float delay)
+    {
+        Destroy(gameObject, delay);
+    }
+}
